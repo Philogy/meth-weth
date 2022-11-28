9176e335fcb6439e6405cf218ea63fe8c7821145
philogy <philippe@dumo.net>
2022-11-28 12:15:00 +0800

    🚧 Add tests

--- test/YAM_WETH.t.sol
+++ test/YAM_WETH.t.sol
@@ -11,7 +11,7 @@ contract YAM_WETH_Test is Test {
 
     event Transfer(address indexed from, address indexed to, uint256 amount);
     event Approval(address indexed owner, address indexed spender, uint256 amount);
-    event PrimaryOperatorSet(address indexed operator);
+    event PrimaryOperatorSet(address indexed account, address indexed prevOperator, address indexed newOperator);
 
     function setUp() public {
         weth = new YAM_WETH();
@@ -40,4 +40,71 @@ contract YAM_WETH_Test is Test {
         assertEq(weth.balanceOf(_account), _amount);
     }
 
+    function testSetOperator(address _account, address _operator1, address _operator2, uint96 _initialBalance) public {
+        vm.assume(_account != address(0));
+        setupBalance(_account, _initialBalance);
+
+        vm.prank(_account);
+        vm.expectEmit(true, true, true, true);
+        emit PrimaryOperatorSet(_account, address(0), _operator1);
+        weth.setPrimaryOperator(_operator1);
+        assertEq(weth.balanceOf(_account), _initialBalance);
+        assertEq(weth.primaryOperatorOf(_account), _operator1);
+
+        vm.prank(_account);
+        vm.expectEmit(true, true, true, true);
+        emit PrimaryOperatorSet(_account, _operator1, _operator2);
+        assertTrue(weth.setPrimaryOperator(_operator2));
+        assertEq(weth.balanceOf(_account), _initialBalance);
+        assertEq(weth.primaryOperatorOf(_account), _operator2);
+    }
+
+    function testTransfer(
+        address _from,
+        address _to,
+        address _fromOperator,
+        address _toOperator,
+        uint96 _fromStartBal,
+        uint96 _toStartBal,
+        uint96 _transferAmount
+    ) public {
+        vm.assume(_from != address(0));
+        vm.assume(_to != address(0));
+        vm.assume(_from != _to);
+        vm.assume(_transferAmount <= _fromStartBal);
+        vm.assume(uint256(_fromStartBal) + uint256(_toStartBal) <= uint256(type(uint96).max));
+        setupBalance(_from, _fromStartBal);
+        setupBalance(_to, _toStartBal);
+        setupOperator(_from, _fromOperator);
+        setupOperator(_to, _toOperator);
+
+        vm.prank(_from);
+        vm.expectEmit(true, true, true, true);
+        emit Transfer(_from, _to, _transferAmount);
+        assertTrue(weth.transfer(_to, _transferAmount));
+        assertEq(weth.balanceOf(_from), _fromStartBal - _transferAmount, "from bal mismatch");
+        assertEq(weth.balanceOf(_to), _toStartBal + _transferAmount, "to bal mismatch");
+        assertEq(weth.primaryOperatorOf(_from), _fromOperator);
+        assertEq(weth.primaryOperatorOf(_to), _toOperator);
+    }
+
+    function calldata1() public {
+        YAM_WETH.Deposit[] memory deposits = new YAM_WETH.Deposit[](3);
+        deposits[0] = YAM_WETH.Deposit(vm.addr(20), 1);
+        deposits[1] = YAM_WETH.Deposit(vm.addr(21), 2);
+        deposits[2] = YAM_WETH.Deposit(vm.addr(21), 3);
+        bytes memory data = abi.encodeCall(YAM_WETH.depositAmountsToMany, (deposits));
+        emit log_named_bytes("data", data);
+    }
+
+    function setupBalance(address _account, uint96 _balance) internal {
+        vm.deal(_account, _balance);
+        vm.prank(_account);
+        weth.deposit{value: _balance}();
+    }
+
+    function setupOperator(address _account, address _operator) internal {
+        vm.prank(_account);
+        weth.setPrimaryOperator(_operator);
+    }
 }
