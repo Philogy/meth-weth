abb2ca7187942d2e5bf1921d7162e1c2ee33d047
Philogy <philippe@dumo.net>
2023-08-01 00:15:30 +0200

    👷 Update libs

--- script/benchmark/Consumers.sol
+++ script/benchmark/Consumers.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.19;
 
 import {IMETH} from "../../src/interfaces/IMETH.sol";
 import {IWETH9} from "../../src/interfaces/IWETH9.sol";
